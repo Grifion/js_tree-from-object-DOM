fa544d024d3a353ce825caf8842386bf5fbb717e
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-18 09:23:22 +0200

    make build

---
+++ index.37db21f9.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,r.append(l),Object.values(t[a]).length>0&&e(l,t[a])}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.37db21f9.js.map
